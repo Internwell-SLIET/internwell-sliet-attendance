a392661647a03b0dc6afeac910e488f2b0f2e866
pratiksha-rathor <pratiksharathor123@gmail.com>
2023-09-22 17:53:54 +0530

    Comment add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <div>
+        {/* Body  */}
         <body className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
           <div
             className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1"
