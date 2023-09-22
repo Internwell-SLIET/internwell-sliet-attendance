96a7a9b8ce753a6a422941fe92917d4e529850cf
rohankumarchaudhary67 <rohankumarchaudhary67@gmail.com>
2023-09-22 14:36:14 +0530

    Index page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,16 @@
 export default function Home() {
   return (
     <div>
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="min-h-screen bg-gray-400 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div
-            className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+            className="absolute inset-0 bg-gradient-to-r from-[#a01f20] to-[#2a5fab] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
           </div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
-              <div>
-                <h1 className="text-2xl font-semibold">Login Form with Floating Labels</h1>
+              <div className="flex flex-col justify-center items-center">
+                <img src="./logo/iw-wbg.png" className="h-24 mb-6" alt="" />
+                <h1 className="text-2xl font-semibold">InternWell Attendace Portal</h1>
               </div>
               <div className="divide-y divide-gray-200">
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
@@ -23,8 +24,8 @@ export default function Home() {
                     <input id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
                     <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                   </div>
-                  <div className="relative">
-                    <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                  <div className="flex flex-col justify-center items-center">
+                    <button className="bg-blue-500 text-white rounded-md px-2 py-1">Login Now</button>
                   </div>
                 </div>
               </div>
