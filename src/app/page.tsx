e735b42cb90cce678515b7ab000f000c6df689b3
pratiksha-rathor <pratiksharathor123@gmail.com>
2023-09-22 14:52:07 +0530

    Adding Comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,12 @@ export default function Home() {
           </div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <div className="max-w-md mx-auto">
-              <div>
-                <h1 className="text-2xl font-semibold">Login Form with Floating Labels</h1>
+              {/* Heading  */}
+              <div className="flex flex-col justify-center items-center">
+                <img src="./logo/iw-wbg.png" className="h-24 mb-6" alt="" />
+                <h1 className="text-2xl font-semibold">InternWell Attendace Portal</h1>
               </div>
+              {/* Form  */}
               <div className="divide-y divide-gray-200">
                 <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                   <div className="relative">
@@ -23,8 +26,9 @@ export default function Home() {
                     <input id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
                     <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                   </div>
-                  <div className="relative">
-                    <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                  {/* Button */}
+                  <div className="flex flex-col justify-center items-center">
+                    <button className="bg-blue-500 text-white rounded-md px-2 py-1">Login Now</button>
                   </div>
                 </div>
               </div>
